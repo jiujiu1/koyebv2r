94740e12b0c1c7e88b7818d3f8b43a8d4077770f
jiujiu1 <q123452322@163.com>
2022-04-20 10:44:12 +0800

    Update xf.sh

--- xf.sh
+++ xf.sh
@@ -60,7 +60,7 @@ cat <<EOF >/etc/v2ray/config.json
             "settings": {
                 "clients": [
                     {
-                        "id": "11111111-1111-1111-1111-111111111111",
+                        "id": "72561d26-4551-4d3e-991d-257f8fd65d2d",
                         "alterId": 0
                     }
                 ],
